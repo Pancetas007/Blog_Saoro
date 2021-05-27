a0d778777ce04f50d197a70fc288ef0b6ca9ab37
Pancetas007 <saoroboltamartil@gmail.com>
2021-05-27 17:55:53 +0200

    Commit 6

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <ul class="menu">
     <li><a href='index.php'>Home</a></li>
     <li><a href='logout.php'>Logout</a></li>
+    <li><a href='register.php'>Register</a></li>
     <li><a href='rss.php'>RSS</a></li>
 </ul>
